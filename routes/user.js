149ad84ae29d2b8e1aec46d75b378095754d1859
gopikrishnan-L <125388612+gopikrishnan-L@users.noreply.github.com>
2024-11-13 21:52:36 +0530

    fix: setting cookie on registration

--- routes/user.js
+++ routes/user.js
@@ -2,19 +2,59 @@ const express = require("express");
 const router = express.Router();
 const axios = require("axios");
 const { userAuth } = require("../middlewares/auth");
+const User = require("../models/user");
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+const dotenv = require("dotenv");
+dotenv.config();
+const jwtSecret = process.env.JWT;
+
+const registerMiddleware = async (req, res, next) => {
+  const { username, password } = req.body;
+  console.log("register" + req.body);
+  if (password.length < 6) {
+    return res.status(400).json({ message: "Password less than 6 characters" });
+  }
+  try {
+    // Hash the password
+    const hash = await bcrypt.hash(password, 10);
+
+    // Create the user with the hashed password
+    const user = await User.create({
+      username,
+      password: hash,
+    });
+    const maxAge = 3 * 60 * 60;
+    const token = jwt.sign(
+      { id: user._id, username, role: user.role },
+      jwtSecret,
+      {
+        expiresIn: maxAge, // 3hrs in sec
+      }
+    );
+    console.log("token in register route ", token);
+    res.cookie("jwt", token, {
+      httpOnly: true,
+      maxAge: maxAge * 1000, // 3hrs in ms
+    });
+
+    req.session.userData = user;
+    next();
+    // Respond with success
+  } catch (error) {
+    // Handle any errors
+    return res.status(400).json({
+      message: "User was not successfully created",
+      error: error.message,
+    });
+  }
+};
 
 router.get("/", (req, res) => res.render("home"));
 router.get("/register", (req, res) => res.render("register"));
 
-router.post("/register", async (req, res) => {
-  const { username, password } = req.body;
+router.post("/register", registerMiddleware, async (req, res) => {
   try {
-    const response = await axios.post("http://localhost:5000/api/auth/register", {
-      username,
-      password,
-    });
-    const data = response.data.user;
-    req.session.userData = data;
     console.log("Cookies:", req.cookies.jwt);
     res.redirect("/otp");
   } catch (error) {
@@ -38,7 +78,11 @@ router.get("/otp", (req, res) => {
 router.post("/otp", async (req, res) => {
   const { otp } = req.body;
   try {
-    const response = await axios.post("http://localhost:5000/api/auth/otp", { otp },{withCredentials:true});
+    const response = await axios.post(
+      "http://localhost:5000/api/auth/otp",
+      { otp },
+      { withCredentials: true }
+    );
     console.log("after otp axios");
     const otpData = response.data;
     if (!req.session.userData) return res.redirect("/login");
